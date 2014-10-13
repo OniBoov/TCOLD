e3296c4c6bac4ce0cb44c0062070500ccf7459d3
Unholychick <lucas__jensen@hotmail.com>
2014-10-13 14:43:45 -0300

    [TW035] - Scripts/Custom: Minesweeper

--- src/server/scripts/Custom/custom_scripts.cpp
+++ src/server/scripts/Custom/custom_scripts.cpp
@@ -9,47 +9,82 @@ enum WeakAlcohol
 
 class TW_spell_item_draenic_pale_ale : public SpellScriptLoader
 {
-public:
-    TW_spell_item_draenic_pale_ale() : SpellScriptLoader("TW_spell_item_draenic_pale_ale") { }
+    public:
+        TW_spell_item_draenic_pale_ale() : SpellScriptLoader("TW_spell_item_draenic_pale_ale") { }
 
-    class TW_spell_item_draenic_pale_ale_SpellScript : public SpellScript
-    {
-        PrepareSpellScript(TW_spell_item_draenic_pale_ale_SpellScript);
-
-        bool Validate(SpellInfo const* /*spellInfo*/) override
+        class TW_spell_item_draenic_pale_ale_SpellScript : public SpellScript
         {
-            if (!sSpellMgr->GetSpellInfo(SPELL_SUMMON_PINK_ELEKK_GUARDIAN) || !sSpellMgr->GetSpellInfo(SPELL_SUMMON_PINK_ELEKK_GUARDIAN))
-                return false;
-            return true;
-        }
+            PrepareSpellScript(TW_spell_item_draenic_pale_ale_SpellScript);
 
-        void HandleScript(SpellEffIndex effIndex)
+            bool Validate(SpellInfo const* /*spellInfo*/) override
+            {
+                if (!sSpellMgr->GetSpellInfo(SPELL_SUMMON_PINK_ELEKK_GUARDIAN) || !sSpellMgr->GetSpellInfo(SPELL_SUMMON_PINK_ELEKK_GUARDIAN))
+                    return false;
+                return true;
+            }
+
+            void HandleScript(SpellEffIndex effIndex)
+            {
+                // If player doesn't already have the mount aura roll for chance of it being applied 
+                if (Player* caster = GetCaster()->ToPlayer())
+                    if (!caster->HasAura(SPELL_PINK_ELEKK_MOUNT))
+                       if (roll_chance_i(33)) // pure guess on 33%
+                       {
+                           PreventHitDefaultEffect(effIndex);
+                           // prevent client crashes from stacking mounts
+                           caster->RemoveAurasByType(SPELL_AURA_MOUNTED);
+                           caster->CastSpell(caster, SPELL_PINK_ELEKK_MOUNT, true, GetCastItem());
+                       }
+            }
+
+            void Register() override
+            {
+                OnEffectHitTarget += SpellEffectFn(TW_spell_item_draenic_pale_ale_SpellScript::HandleScript, EFFECT_1, SPELL_EFFECT_TRIGGER_SPELL);
+            }
+        };
+
+        SpellScript* GetSpellScript() const override
         {
-            // If player doesn't already have the mount aura roll for chance of it being applied 
-            if (Player* caster = GetCaster()->ToPlayer())
-                if (!caster->HasAura(SPELL_PINK_ELEKK_MOUNT))
-                    if (roll_chance_i(33)) // pure guess on 33%
-                    {
-                        PreventHitDefaultEffect(effIndex);
-                        // prevent client crashes from stacking mounts
-                        caster->RemoveAurasByType(SPELL_AURA_MOUNTED);
-                        caster->CastSpell(caster, SPELL_PINK_ELEKK_MOUNT, true, GetCastItem());
-                    }
+            return new TW_spell_item_draenic_pale_ale_SpellScript();
         }
+};
 
-        void Register() override
+class TW_spell_landmine_knockback : public SpellScriptLoader
+{
+    public:
+        TW_spell_landmine_knockback() : SpellScriptLoader("TW_spell_landmine_knockback") { }
+
+        class TW_spell_landmine_knockback_SpellScript : public SpellScript
         {
-            OnEffectHitTarget += SpellEffectFn(TW_spell_item_draenic_pale_ale_SpellScript::HandleScript, EFFECT_1, SPELL_EFFECT_TRIGGER_SPELL);
-        }
-    };
+            PrepareSpellScript(TW_spell_landmine_knockback_SpellScript);
 
-    SpellScript* GetSpellScript() const override
-    {
-        return new TW_spell_item_draenic_pale_ale_SpellScript();
-    }
+            void HandleScript(SpellEffIndex /*effIndex*/)
+            {
+                if (Player* target = GetHitPlayer())
+                {
+                    Aura const* aura = GetHitAura();
+                    if (!aura || aura->GetStackAmount() != 10)
+                        return;
+
+                    AchievementEntry const* achiev = sAchievementStore.LookupEntry(1428);
+                    target->CompletedAchievement(achiev);
+                }
+            }
+
+            void Register() override
+            {
+                OnEffectHitTarget += SpellEffectFn(TW_spell_landmine_knockback_SpellScript::HandleScript, EFFECT_1, SPELL_EFFECT_APPLY_AURA);
+            }
+        };
+
+        SpellScript* GetSpellScript() const override
+        {
+            return new TW_spell_landmine_knockback_SpellScript();
+        }
 };
 
 void AddSC_custom_scripts()
 {
     new TW_spell_item_draenic_pale_ale();
+    new TW_spell_landmine_knockback();
 }
