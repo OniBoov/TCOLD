da43fa8087c76c0f5abb99d5cf133a3074e1ac2e
Gecko32 <Gecko3262@gmail.com>
2014-10-15 02:33:40 -0300

    [TW057] - Core/Spells: Spirit Heal - abilities also have no cost

--- src/server/game/Spells/SpellMgr.cpp
+++ src/server/game/Spells/SpellMgr.cpp
@@ -3333,6 +3333,8 @@ void SpellMgr::LoadSpellInfoCorrections()
                 break;
             case 59630: // Black Magic
                 spellInfo->Attributes |= SPELL_ATTR0_PASSIVE;
+            case 44535: // Spirit Heal, abilities also have no cost
+                spellInfo->Effects[EFFECT_0].MiscValue = 127;
                 break;
             case 17364: // Stormstrike
                 spellInfo->AttributesEx3 |= SPELL_ATTR3_STACK_FOR_DIFF_CASTERS;
