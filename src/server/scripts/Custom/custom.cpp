823706cbc89b28d254be767ce9566a23bf110735
Nyeriah <sarah.trysan@live.com>
2014-10-19 15:35:45 -0200

    [TW083] - Scripts/Custom: Add Thorim's file

--- src/server/scripts/Custom/custom.cpp
+++ src/server/scripts/Custom/custom.cpp
@@ -2,6 +2,7 @@
 
 /* This is where custom scripts' loading functions should be declared. */
 void AddSC_custom_scripts();
+void AddSC_TW_boss_thorim();
 
 /* This is where custom scripts should be added. */
 void AddSC_custom()
