7a41d9384e86e31865fb6cb1ead8adc9f16ce489
Nyeriah <sarah.trysan@live.com>
2014-11-10 00:04:32 -0200

    [TW128] Scripts/ToC5: Add custom instance files

--- src/server/scripts/Custom/custom.cpp
+++ src/server/scripts/Custom/custom.cpp
@@ -2,6 +2,11 @@
 
 /* This is where custom scripts' loading functions should be declared. */
 void AddSC_custom_scripts();
+void AddSC_TW_instance_trial_of_the_champion();
+void AddSC_TW_boss_grand_champions();
+void AddSC_TW_boss_black_knight();
+void AddSC_TW_boss_argent_challenge();
+void AddSC_TW_trial_of_the_champion();
 void AddSC_TW_boss_thorim();
 void AddSC_custom_commandscript();
 
@@ -10,5 +15,10 @@ void AddSC_custom()
 {
     AddSC_custom_commandscript();
     AddSC_custom_scripts();
+    AddSC_TW_instance_trial_of_the_champion();
+    AddSC_TW_boss_argent_challenge();
+    AddSC_TW_boss_black_knight();
+    AddSC_TW_boss_grand_champions();
+    AddSC_TW_trial_of_the_champion();
     AddSC_TW_boss_thorim();
 }
